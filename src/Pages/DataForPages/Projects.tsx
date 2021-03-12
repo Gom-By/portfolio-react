434c6e4877820914a8c196addb409c5ea55c1e77
Guerby Naharro <naharroguerby59@gmail.com>
2021-03-12 14:30:30 +0100

    updates

---
+++ src/Pages/DataForPages/Projects.tsx
@@ -0,0 +1,23 @@
+export const project = [
+    {
+        id: 0,
+        title: "basic todolist",
+        description: "this project is a basci todolist in vanilla javascript who set all todos in  the local storage. It's notfinish....",
+        img: "https://",
+        link: "https://gom-by.github.io/todo"
+    },
+    {
+        id: 1,
+        title: "title of the project",
+        description: "description of the project",
+        img: "https://",
+        link: "https://gom-by.github.io/project"
+    },
+    {
+        id: 2,
+        title: "title of the project",
+        description: "description of the project",
+        img: "https://",
+        link: "https://gom-by.github.io/project"
+    }
+]
