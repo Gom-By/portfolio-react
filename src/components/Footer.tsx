9806472ac09dbc4d71d688e2d4c61ad650121b2b
Guerby Naharro <naharroguerby59@gmail.com>
2021-03-12 20:29:45 +0100

    updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,14 @@ const Footer = () => {
         <div className="text-center font-lg bg-gray-800 text-white p-9 h-60 text-lg">
             <h1>Hello this is my footer</h1>
             <p>See my account on <a href="https://github.com/Gom-By" className="text-blue-400 underline">Github</a></p>
+            <ul>
+                <li>
+                    <a className="text-center" href="https://reactjs.org">
+                        <img className="h-9 w-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png" alt="none"></img>
+                    </a>
+                </li>
+                <li><a href="https://tailwindcss.com">Tailwindcss</a></li>
+            </ul>
         </div>
     )
 }
---
+++ src/components/Languages.tsx
@@ -0,0 +1,7 @@
+export const languages = [
+    {
+        name: "React",
+        site: "https://fr.reactjs.org",
+        imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"
+    }
+]
