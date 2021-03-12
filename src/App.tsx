72d469b8ace8b4b370ff132ffb04eda644717253
Guerby Naharro <naharroguerby59@gmail.com>
2021-03-12 12:06:17 +0100

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import './App.css';
-import React from 'react';
-import logo from './logo.svg';
+import './assets/main.css';
+import Header from './components/Header';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Route path="/" exact>
+        <p>Hello in home page</p>
+      </Route>
+      <Route path="/about">
+        <p>Hello in about page</p>
+      </Route>
+      <Route path="/contact" exact>
+        <p>Hello in contact page</p>
+      </Route>
+    </Router>
   );
 }
+
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+    return (
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/about">Home</Link>
+            <Link to="/contact">Home</Link>
+        </div>
+    )
+}
+
+export default NavBar
