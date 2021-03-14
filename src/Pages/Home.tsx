b03c0faad2993b53b89985b059727c2f907beeb6
Guerby Naharro <naharroguerby59@gmail.com>
2021-03-14 18:54:18 +0100

    create api

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import { projects, projectTailwindcss } from './DataForPages/Projects'
+import React, { useState, useEffect } from 'react'
+import { projectsComponent } from './DataForPages/Projects'
+import axios from 'axios'
 
-const projectsComponent = projects.map((project) => {
-    return (
-        <div className={projectTailwindcss.container} key={project.id}>
-            <div>
-                <h1 className={projectTailwindcss.h1}>{project.title}</h1>
-                <p className={projectTailwindcss.p}>{project.description}</p>
-            </div>
-            <div>
-                <a className={projectTailwindcss.a} href={project.link}>Try the code</a>
-                <img className={projectTailwindcss.img} src={project.imgUrl} alt="reload" ></img>
-            </div>
-        </div>
-    )
-})
 const Home = () => {
+    const [elements, setElement] = useState<any[]>([])
+    useEffect(() => {
+        axios
+            .get("https://api.github.com/users/Gom-By/repos")
+            .then(res => {
+                setElement(res.data)
+            })
+    }, [])
+
     return (
         <div className="containerComponent">
+            {elements.map(element => {
+                return (
+                    <div key={element.id}>
+                        <h1>{element.name}</h1>
+                        <p>buil with the {element.language}</p>
+                        <a href={element.html_url}>link to the repo</a>
+                    </div>
+                )
+            })}
             {projectsComponent}
         </div>
     )
