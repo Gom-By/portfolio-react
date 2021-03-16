a8659429457c72b5533fa0c656c0b6e98620c37b
Guerby Naharro <naharroguerby59@gmail.com>
2021-03-16 10:15:15 +0100

    updates

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,24 +1,9 @@
-import React from 'react'
-import { projects, projectTailwindcss } from './DataForPages/Projects'
+import Projects from "./DataForPages/Projects"
 
-const projectsComponent = projects.map((project) => {
-    return (
-        <div className={projectTailwindcss.container} key={project.id}>
-            <div>
-                <h1 className={projectTailwindcss.h1}>{project.title}</h1>
-                <p className={projectTailwindcss.p}>{project.description}</p>
-            </div>
-            <div>
-                <a className={projectTailwindcss.a} href={project.link}>Try the code</a>
-                <img className={projectTailwindcss.img} src={project.imgUrl} alt="reload" ></img>
-            </div>
-        </div>
-    )
-})
 const Home = () => {
     return (
         <div className="containerComponent">
-            {projectsComponent}
+            <Projects />
         </div>
     )
 }
