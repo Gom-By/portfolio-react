a5c9fbb9b6e5dce7c639b46bb1bfe376ce32e2d8
Gomby <naharroguerby59@gmail.com>
2024-04-08 18:40:06 +0200

    Saved last changes on css

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div className="containerComponent">
-            <p>Here going to be a form</p>
+        <div className="container" id="contact">
+            <h2 className="h1-container text-4xl font-pacifico">If you want to contact me....</h2>
+            <div id="box-contact" className="flex justify-center items-center bg-black rounded-lg mx-10 md:mx-0">
+                <div className="link-reseaux w-1/2 md:w-auto border-r border-blue-500">
+                    <ul id="list-reseaux" className="text-3xl text-lightblue text-center py-15%">
+                        <li className="item-reseaux my-6"><a href="http://github.com/Gom-By" id="link-github">GitHub</a></li>
+                        <li className="item-reseaux my-6"><a href="http://discord.com" id="link-discord">Discord</a></li>
+                        <li className="item-reseaux my-6"><a href="#" id="link-insta">Instagram</a></li>
+                        <li className="item-reseaux my-6"><a href="#" id="link-snap">Snapchat</a></li>
+                    </ul>
+                </div>
+                <p id="form-contact" className="text-3xl text-white border-l border-red-500 pl-10 md:pl-0 py-10 md:py-0 w-1/2 md:w-auto">a future form container</p>
+            </div>
         </div>
     )
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,15 +3,17 @@ import TailwindLogo from './TailwindLogo'
 
 const Footer = () => {
     return (
-        <div className="select-none grid justify-around text-center bg-gray-800 text-white p-9 h-full md:h-52">
-            <p className="font-serif text-2xl mb-8 md:my-3">There are languages that I use in this project</p>
-            <div className="flex flex-col md:flex-row  justify-around px-16">
-                <a href="https://github.com/Gom-By" className="text-blue-400 underline"><img className="h-9 w-11" src="https://th.bing.com/th/id/R724794164fb289dd2f7d69dde7ac3bc0?rik=0Ubh3aP6JzCPcw&riu=http%3a%2f%2fpngimg.com%2fuploads%2fgithub%2fgithub_PNG40.png&ehk=vDH1g6b2G5qphfQR7RsUJ7HmqSSwIMycien%2fvBj03ZU%3d&risl=&pid=ImgRaw" alt="none"></img></a>
-                <a href="https://tailwindcss.com"><TailwindLogo /></a>
-                <a href="https://reactjs.org"><img className="h-9 w-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png" alt="none"></img></a>
-                <a href="https://typescriptlang.org"><img className="h-9 w-12" src="https://th.bing.com/th/id/R7f7aa78e1d5e3e0fbc6d8239cc5a3f18?rik=WrXKuQ4pSfq%2bRg&riu=http%3a%2f%2fwww.yanelyramirez.com%2fimages%2ficons%2ftypescript.png&ehk=3nqIs6BiHhjL1fbqeQxI2uMnsxjjEYfxDe3jMFN6Az4%3d&risl=&pid=ImgRaw" alt="none"></img></a>
-            </div>
-        </div>
+        <footer id="footer" className="h-25vh text-4xl text-center bg-gray-300 mt-100px pt-25px border-t border-black">
+            <p className="p-short"> 
+                <p className="font-serif text-2xl mb-8 md:my-3">There are languages that I use in this project</p>
+                <div className="flex flex-col md:flex-row  justify-around px-16">
+                    <a href="https://github.com/Gom-By" className="text-blue-400 underline"><img className="h-9 w-11" src="https://th.bing.com/th/id/R724794164fb289dd2f7d69dde7ac3bc0?rik=0Ubh3aP6JzCPcw&riu=http%3a%2f%2fpngimg.com%2fuploads%2fgithub%2fgithub_PNG40.png&ehk=vDH1g6b2G5qphfQR7RsUJ7HmqSSwIMycien%2fvBj03ZU%3d&risl=&pid=ImgRaw" alt="none"></img></a>
+                    <a href="https://tailwindcss.com"><TailwindLogo /></a>
+                    <a href="https://reactjs.org"><img className="h-9 w-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png" alt="none"></img></a>
+                    <a href="https://typescriptlang.org"><img className="h-9 w-12" src="https://th.bing.com/th/id/R7f7aa78e1d5e3e0fbc6d8239cc5a3f18?rik=WrXKuQ4pSfq%2bRg&riu=http%3a%2f%2fwww.yanelyramirez.com%2fimages%2ficons%2ftypescript.png&ehk=3nqIs6BiHhjL1fbqeQxI2uMnsxjjEYfxDe3jMFN6Az4%3d&risl=&pid=ImgRaw" alt="none"></img></a>
+                </div>
+            </p>
+        </footer>
     )
 }
 
